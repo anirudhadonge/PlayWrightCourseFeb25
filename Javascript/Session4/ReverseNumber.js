bec426e238d7d976606406966bbd25ddf867fe18
anirudha <anirudhadonge@hotmail.com>
2025-02-26 09:18:53 +0530

    Session11

--- Javascript/Session4/ReverseNumber.js
+++ Javascript/Session4/ReverseNumber.js
@@ -1,17 +1,18 @@
+/// javascript number type : integer, decimal value, 
+/// let a  = 12345/10 = 1234.5
+let n1 = 123456;
+let n2 =0;
 
-// let n1 = 123456;
-// let n2 =0;
+while(n1>0){
 
-// while(n1>0){
+    n2 = n2*10 +n1%10;
+    n1 = n1/10-(n1%10)/10; // 12345.6 - 0.6 = 0
+    //console.log(n1);
+}
 
-//     n2 = n2*10 +n1%10;
-//     n1 = n1/10-(n1%10)/10; // 12345.6 - 0.6 = 12345
-//     console.log(n1);
-// }
-
-// console.log(n2);
+console.log(n2);
 
 
-0,1,1,2,3,5,8,13,21,34
+// 0,1,1,2,3,5,8,13,21,34
 
-userInput = 50 ,   
+// userInput = 50 ,   
