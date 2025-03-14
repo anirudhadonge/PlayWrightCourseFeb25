de52535cffde64a82f42dd68e72c8f20157d2007
anirudha <anirudhadonge@hotmail.com>
2025-03-14 06:03:15 +0530

    Session 13-03-2025

--- PlaywrightProjectDemo/tests/FirstTest.spec.ts
+++ PlaywrightProjectDemo/tests/FirstTest.spec.ts
@@ -12,3 +12,31 @@ test("this is my first test",async ({page})=>{
         await page.locator(".getStarted_Sjon").click();
         await expect(page.locator('.theme-doc-markdown.markdown h1')).toHaveText("Installation");
 })
+
+
+test("Locator Description test",async({page})=>{
+        await page.goto("https://the-internet.herokuapp.com/")
+        const checkBox = page.locator('[href="/checkboxes"]')
+        await checkBox.click();
+})
+
+
+test('Fill and Press-Sequentially test',async({page})=>{
+        await page.goto("https://the-internet.herokuapp.com/login");
+        await page.locator("#username").pressSequentially("tomsmith",{delay:1000});
+        await page.locator("#password").fill("SuperSecretPassword!")
+})
+
+test.only('Select Options test',async({page})=>{
+        await page.goto("https://the-internet.herokuapp.com/dropdown");
+        await page.locator("select#dropdown").selectOption({index:2});
+        // await page.locator("select#dropdown").click();
+        // await page.locator("select#dropdown option:has-text('Option 2')").click();
+        
+})
+
+
+// value 
+
+// Label
+
