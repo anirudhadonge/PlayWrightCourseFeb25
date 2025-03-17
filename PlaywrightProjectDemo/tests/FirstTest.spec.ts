fe57cfde9743837dbf0cf6499f4b502b2568508b
anirudha <anirudhadonge@hotmail.com>
2025-03-17 14:23:05 +0530

    download and frames session

--- PlaywrightProjectDemo/tests/FirstTest.spec.ts
+++ PlaywrightProjectDemo/tests/FirstTest.spec.ts
@@ -1,5 +1,15 @@
-import {test, expect} from '@playwright/test'
-
+import { test, expect } from "@playwright/test";
+/**
+ * 		○ Checkboxes and radio buttons
+		○ Upload files
+		○ Frames
+		○ Download File
+		○ Javascript Popups 
+		○ Hover
+		○ New Page
+		○ Basic Authentication
+                Drag and Drop
+ */
 
 /**
  * Browser
@@ -7,36 +17,97 @@ import {test, expect} from '@playwright/test'
  * Page
  */
 //Browser---> Brwoser Context---> page
-test("this is my first test",async ({page})=>{
-        await page.goto("https://playwright.dev/");
-        await page.locator(".getStarted_Sjon").click();
-        await expect(page.locator('.theme-doc-markdown.markdown h1')).toHaveText("Installation");
+test("this is my first test", async ({ page }) => {
+  await page.goto("https://playwright.dev/");
+  await page.locator(".getStarted_Sjon").click();
+  await expect(page.locator(".theme-doc-markdown.markdown h1")).toHaveText(
+    "Installation"
+  );
+});
+
+test("Locator Description test", async ({ page }) => {
+  await page.goto("https://the-internet.herokuapp.com/");
+  const checkBox = page.locator('[href="/checkboxes"]');
+  await checkBox.click();
+});
+
+test("Fill and Press-Sequentially test", async ({ page }) => {
+  await page.goto("https://the-internet.herokuapp.com/login");
+  await page
+    .locator("#username")
+    .pressSequentially("tomsmith", { delay: 1000 });
+  await page.locator("#password").fill("SuperSecretPassword!");
+});
+
+test("Select Options test", async ({ page }) => {
+  await page.goto("https://the-internet.herokuapp.com/dropdown");
+  await page.locator("select#dropdown").selectOption({ index: 2 });
+  await page.locator("select#dropdown").click();
+  await page.locator("select#dropdown option:has-text('Option 2')").click();
+});
+
+/**
+ * check() ---> validate that the element is not checked then perform check action
+ * uncheck() ---> validate that the element is checked then perform unchecking action
+ * Click();
+ */
+
+//WebElement element = driver.findElement(By.Css(""))
+test.only('Checkbox test', async({page})=>{
+        await page.goto("https://the-internet.herokuapp.com");
+        await page.getByText('Checkboxes').click();
+        //await page.locator("[href='/checkboxes']").click();
+        // if(!await page.locator("#checkboxes input").nth(0).isChecked()){
+        //         await page.locator("#checkboxes input").nth(0).click();
+        // }
+        // if(await page.locator("#checkboxes input").nth(1).isChecked()){
+        //         await page.locator("#checkboxes input").nth(1).click();
+        // }
+        // await page.locator("#checkboxes input").nth(0).uncheck();
+        // await page.locator("#checkboxes input").nth(1).uncheck();
+        await page.getByText(' checkbox 1').check();
+        await page.getByText(' checkbox 2').uncheck();
 })
 
+test.only('File Upload test',async({page})=>{
+        await page.goto("https://the-internet.herokuapp.com");
+        await page.locator("[href='/upload']").click();
+        // await page.locator("#file-upload").setInputFiles(["C:/UploadFiles/downloadFile.txt",'C:/UploadFiles/Uploadfile.txt']);
+        // await page.locator("#file-submit").click();
 
-test("Locator Description test",async({page})=>{
-        await page.goto("https://the-internet.herokuapp.com/")
-        const checkBox = page.locator('[href="/checkboxes"]')
-        await checkBox.click();
+        // When we have a button which chooses the file is not a input(its not a input tag)
+        const fileChooser = page.waitForEvent('filechooser');
+        await page.locator("#file-upload").click();
+        const fchooser = await fileChooser;
+        await fchooser.setFiles('C:/UploadFiles/Uploadfile.txt');
+        await page.locator("#file-submit").click();
 })
 
-
-test('Fill and Press-Sequentially test',async({page})=>{
-        await page.goto("https://the-internet.herokuapp.com/login");
-        await page.locator("#username").pressSequentially("tomsmith",{delay:1000});
-        await page.locator("#password").fill("SuperSecretPassword!")
+test.only('Working with Iframes ',async({page})=>{
+        await page.goto("https://the-internet.herokuapp.com/");
+        await page.locator("[href='/frames']").click();
+        await page.locator("[href='/iframe']").click();
+        const floc = page.frameLocator('#mce_0_ifr');
+        await expect(floc.locator('#tinymce p')).toHaveText("Your content goes here.");
+        await expect(page.locator(".example h3")).toHaveText('An iFrame containing the TinyMCE WYSIWYG Editor');
 })
 
-test.only('Select Options test',async({page})=>{
-        await page.goto("https://the-internet.herokuapp.com/dropdown");
-        await page.locator("select#dropdown").selectOption({index:2});
-        // await page.locator("select#dropdown").click();
-        // await page.locator("select#dropdown option:has-text('Option 2')").click();
-        
-})
+test.only("Working Multiple Frames", async ({ page }) => {
+  await page.goto("https://the-internet.herokuapp.com/");
+  await page.locator("[href='/frames']").click();
+  await page.locator("[href='/nested_frames']").click();
+  const topFrame = page.frameLocator('[name="frame-top"]');
+  const leftFrame = topFrame.frameLocator('[name="frame-left"]')
+  await expect(leftFrame.locator('body')).toContainText('LEFT')
+  const midFrame = topFrame.frameLocator('[name="frame-middle"]');
+  await expect(midFrame.locator('#content')).toHaveText('MIDDLE')
+});
 
-
-// value 
-
-// Label
-
+test.only("Working with Download files ", async ({ page }) => {
+  await page.goto("https://the-internet.herokuapp.com/");
+  await page.locator("[href='/download']").click();
+  const downloadEvent = page.waitForEvent("download");
+  await page.locator("[href='download/java.png']").click();
+  const downloadFile = await downloadEvent;
+  await downloadFile.saveAs("./Downloads/" + downloadFile.suggestedFilename());
+});
